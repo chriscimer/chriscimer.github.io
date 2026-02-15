f43a5bc2d890fe7c05822563587605dadb05ffc6
chriscimer <chriscimer@icloud.com>
2026-02-15 11:08:18 +0000

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -118,22 +118,5 @@ prevBtn.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
   modalImg.src = galleryImages[currentIndex].src;
 });
-  // ===== FADE IN ON SCROLL =====
-
-const portfolioImages = document.querySelectorAll('#portfolio .gallery img');
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {
-  threshold: 0.15
-});
-
-portfolioImages.forEach(img => {
-  observer.observe(img);
-});
 
 });
